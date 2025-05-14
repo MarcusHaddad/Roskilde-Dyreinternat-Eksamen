3de38dcb471f3528dd2ea401dda82e0992491bf6
Emre Can Caliskan <emc004@edu.zealand.dk>
2025-05-14 11:19:34 +0200

    .

--- ClassLibrary Dyreinternat/Medarbejdere.cs
+++ ClassLibrary Dyreinternat/Medarbejdere.cs
@@ -2,6 +2,12 @@
 {
     public class Medarbejdere
     {
+        public int Id { get; set; }
 
+        public string FullName { get; set; }
+
+        public string Role { get; set; } // Fx "Medarbejder" eller "Bestyrelse" 
+
+        public string Email { get; set; }
     }
 }
