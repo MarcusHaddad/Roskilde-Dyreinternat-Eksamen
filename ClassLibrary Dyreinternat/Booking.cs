1351f4dd4c8ca88e293381d2cd4e06cb02490c40
Marcus Michel Haddad <mah045@edu.zealand.dk>
2025-05-14 11:44:32 +0200

    ..

---
+++ ClassLibrary Dyreinternat/Booking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_Dyreinternat
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public DateTime BookingTime { get; set; }
+
+        // Relation til kunde
+        public int KunderId { get; set; }
+        public Kunder Kunder { get; set; }
+
+        // Relation til dyr (valgfrit)
+        public int? AnimalId { get; set; }
+        public Animal Animal { get; set; }
+    }
+}
+    
+
