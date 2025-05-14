c3c56851145a52ca50512a484276df6baab60006
Marcus Michel Haddad <mah045@edu.zealand.dk>
2025-05-14 11:22:11 +0200

    .

--- ClassLibrary Dyreinternat/Log.cs
+++ ClassLibrary Dyreinternat/Log.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary_Dyreinternat
 {
-    internal class Log
+    public class VisitLog
     {
+        public int Id { get; set; }
+        public DateTime Date { get; set; } // Dato for besøget
+        public string VisitorName { get; set; } // Navn på kunde eller dyrlæge
+        public string Note { get; set; } // F.eks. "Vaccination" eller "Adoptionsbesøg"
+
+        // Relation til dyret
+        public int AnimalId { get; set; }
+        public Animal Animal { get; set; }
     }
+
 }
