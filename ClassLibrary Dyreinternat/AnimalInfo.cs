d2b9561d437c1ef75629a052dd910a557e14018c
Marcus Michel Haddad <mah045@edu.zealand.dk>
2025-05-14 11:10:11 +0200

    .

---
+++ ClassLibrary Dyreinternat/AnimalInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_Dyreinternat
+{
+    public class Dog : Animal
+    {
+        public Dog() { Species = "Hund"; }
+    }
+
+    public class Cat : Animal
+    {
+        public Cat() { Species = "Kat"; }
+    }
+
+    public class Rabbit : Animal
+    {
+        public Rabbit() { Species = "Kanin"; }
+    }
+
+    public class Bird : Animal
+    {
+        public Bird() { Species = "Fugl"; }
+    }
+
+    public class GuineaPig : Animal
+    {
+        public GuineaPig() { Species = "Marsvin"; }
+    }
+
+}
