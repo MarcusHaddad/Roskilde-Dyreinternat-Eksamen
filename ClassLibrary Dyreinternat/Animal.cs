63fb1e0522f08bd67a1d867eadd470ea01ac8eb1
FreddyBZealand <frb009@edu.zealand.dk>
2025-05-14 11:28:24 +0200

    .

--- ClassLibrary Dyreinternat/Animal.cs
+++ ClassLibrary Dyreinternat/Animal.cs
@@ -11,7 +11,7 @@ namespace ClassLibrary_Dyreinternat
 
         #region Instance fields
         int _id;
-       public string _name;
+        string _name;
         string _species;
         string _chipNumber;
         string _breed;
@@ -32,17 +32,56 @@ namespace ClassLibrary_Dyreinternat
             _birthYear = birthYear;
             _medicalnote = medicalnote;
         }
+
+
+        #endregion
+
+        #region Properties
+        private int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        private string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        private string Species
+        {
+            get { return _species; }
+            set { _species = value; }
+
+        }
+        private string Breed
+        {
+            get { return _breed; }
+            set { _breed = value; }
+        }
+        private string Size
+        {
+            get { return _size; }
+            set { _size = value; }
+        }
+        private int BirthYear
+        {
+            get { return _birthYear; }
+            set { _birthYear = value; }
+        }
+        private string Medicalnote
+        {
+            get { return _medicalnote; }
+            set { _medicalnote = value; }
+        }
+
+        #endregion
+
+        #region Methods
+
+
+        #endregion
+
     }
-
-    #endregion
-
-    #region
-    #endregion
-
-    #region
-    #endregion
-
-
 }
 
 
