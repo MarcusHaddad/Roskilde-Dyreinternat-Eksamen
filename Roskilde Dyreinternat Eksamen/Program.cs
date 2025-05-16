68ab427fdc8c0434d240b500c7155c6b3211aac4
FreddyBZealand <frb009@edu.zealand.dk>
2025-05-16 11:22:05 +0200

    .

--- Roskilde Dyreinternat Eksamen/Program.cs
+++ Roskilde Dyreinternat Eksamen/Program.cs
@@ -10,7 +10,13 @@ namespace Roskilde_Dyreinternat_Eksamen
             {
                 new Animal(12, "Milo", "Dog", "12201200", "Golden retriever", "Big", "2015", "good medicals"),
                 new Animal(17, "Karl", "Rabbit", "156700", "Giant rabbit", "Big", "2021", "good medicals"),
-                new Animal(21, "Frank", "Cat", "150055", "black cat", "small", "2024", "good medicals")
+                new Animal(21, "Frank", "Cat", "150055", "black cat", "small", "2024", "good medicals"),
+                new Animal(124, "Norm", "Bird", "150981", "Parrot", "small", "2020", "good medicals"),
+                new Animal(124, "Norm", "Bird", "150981", "Parrot", "small", "2020", "good medicals"),
+                new Animal(),
+                new Animal(),
+
+
             };
             foreach (var animal in animals)
             {
@@ -22,3 +28,4 @@ namespace Roskilde_Dyreinternat_Eksamen
         }
     }
 }
+
