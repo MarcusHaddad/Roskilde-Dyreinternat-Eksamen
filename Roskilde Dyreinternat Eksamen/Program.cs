5c99c7a37ae3377e9aa12afbc60778356efdcb79
FreddyBZealand <frb009@edu.zealand.dk>
2025-05-16 11:16:16 +0200

    .

--- Roskilde Dyreinternat Eksamen/Program.cs
+++ Roskilde Dyreinternat Eksamen/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Roskilde_Dyreinternat_Eksamen
+﻿using ClassLibrary_Dyreinternat;
+using System;
+namespace Roskilde_Dyreinternat_Eksamen
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Animal> animals = new List<Animal>
+            {
+                new Animal(12, "Milo", "Dog", "12201200", "Golden retriever", "Big", "2015", "good medicals"),
+                new Animal(17, "Karl", "Rabbit", "156700", "Giant rabbit", "Big", "2021", "good medicals"),
+                new Animal(21, "Frank", "Cat", "150055", "black cat", "small", "2024", "good medicals")
+            };
+            foreach (var animal in animals)
+            {
+                animal.VisInfo();
+            }
+                Console.ReadLine();
+                Console.WriteLine("Hello, World!");
+            
         }
     }
 }
