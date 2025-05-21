0a11c2bc0f55a0b0a189afef597e9502a872c2a0
Marcus Michel Haddad <mah045@edu.zealand.dk>
2025-05-21 10:40:54 +0200

    ...

--- Roskilde Dyreinternat Eksamen/Program.cs
+++ Roskilde Dyreinternat Eksamen/Program.cs
@@ -8,14 +8,14 @@ namespace Roskilde_Dyreinternat_Eksamen
         {
             List<Animal> animals = new List<Animal>
             {
-                new Animal(12, "Milo", "Dog", "12201200", "Golden retriever", "Big", "2015", "good medicals"),
-                new Animal(17, "Karl", "Rabbit", "156700", "Giant rabbit", "Big", "2021", "good medicals"),
-                new Animal(21, "Frank", "Cat", "150055", "black cat", "small", "2024", "good medicals"),
+                new Animal(12, "Milo", "Dog1", "12201200", "Golden retriever", "Big", "2015", "good medicals"),
+                new Animal(17, "Karl", "Rabbit1", "156700", "Giant rabbit", "Big", "2021", "good medicals"),
+                new Animal(21, "Frank", "Cat1", "150055", "black cat", "small", "2024", "good medicals"),
                 new Animal(124, "Norm", "Bird", "150981", "Parrot", "small", "2020", "good medicals"),
                 new Animal(1242, "Gordon", "Guineapig", "202012", "Sheltie", "small", "2025", "good medicals"),
-                new Animal(1242, "Molly", "Cat", "202695", "Persian", "medium", "2019", "good medicals"), 
-                new Animal(1580, "Carlo", "Dog", "80584030", "Siberian Husky", "Big", "2023", "good medicals"),               
-                new Animal(1242, "Bertha", "Rabbit", "281960", "polish rabbit", "small", "2022", "good medicals"),
+                new Animal(1242, "Molly", "Cat2", "202695", "Persian", "medium", "2019", "good medicals"), 
+                new Animal(1580, "Carlo", "Dog2", "80584030", "Siberian Husky", "Big", "2023", "good medicals"),               
+                new Animal(1242, "Bertha", "Rabbit2", "281960", "polish rabbit", "small", "2022", "good medicals"),
 
 
             };
